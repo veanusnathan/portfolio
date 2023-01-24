b6c3bcf0fc6ca554aaaf748c2ae20596187dbdc7
veanusnathan <veanusnathan.work@gmail.com>
2023-01-25 05:30:00 +0700

    Portfolio 0.1.1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import About from "../components/About";
+import Experience from "../components/Experience";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Skills from "../components/Skills";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -10,22 +12,19 @@ export default function Home() {
 			<Head>
 				<title>Veanus Nathan Portfolio</title>
 			</Head>
-			{/* Header */}
 			<Header />
-
-			{/* Hero */}
 			<section id="hero" className="snap-start scroll-smooth">
 				<Hero />
 			</section>
-
-			{/* About */}
 			<section id="about" className="snap-center scroll-smooth">
 				<About />
 			</section>
-
-			{/* Experience */}
-
-			{/* Skills */}
+			<section id="experience" className="snap-center">
+				<Experience />
+			</section>
+			<section className="snap-start">
+				<Skills />
+			</section>
 
 			{/* Projects */}
 
